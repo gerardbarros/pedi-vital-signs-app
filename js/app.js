20ba616bff59efe7a9a62bc8eb7375b21bc0591f
Gerard <barros.gc@gmail.com>
2025-04-07 21:25:49 -0500

    Update API URL.

--- js/app.js
+++ js/app.js
@@ -4,10 +4,10 @@ async function apiRequest(){
         const response = await fetch(`https://pedi-vital-signs-api.onrender.com/api/${age}`)
         const data = await response.json()
 
-        document.querySelector(".heart-rate").innerText = `${data.heartRate} bpm`
-        document.querySelector(".respiratory-rate").innerText = `${data.respiratoryRate} breaths/min`
-        document.querySelector(".systolic").innerText = `${data.systolic} mmHg`
-        document.querySelector(".diastolic").innerText = `${data.diastolic} mmHg`
+        document.querySelector(".heart-rate").innerText = data.heartRate + " bpm"
+        document.querySelector(".respiratory-rate").innerText = data.respiratoryRate + " breaths/min"
+        document.querySelector(".systolic").innerText = data.systolic + " mmHg"
+        document.querySelector(".diastolic").innerText = data.diastolic + " mmHg"
     } catch(error){
         console.log(error)
     }
